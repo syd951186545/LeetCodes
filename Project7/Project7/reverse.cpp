905129be95b2556b14b65fd1604372a830350501
syd951186545 <951186545@qq.com>
2018-09-01 23:40:13 +0800

    project 7

---
+++ Project7/Project7/reverse.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int reverse(int x) {
+		if (-9 < x < 9) { return x };
+		while (x)
+		{
+			int a = (x % 10);
+			int x = (x - a) / 10;
+			int b = b +(10 * a);
+		}
+		
+	}
+};
